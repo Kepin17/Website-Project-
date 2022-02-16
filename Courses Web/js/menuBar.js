1aa460da53a6c4f2c4521121337ae736712569e1
Kevien.OJ <69498060+Kepin17@users.noreply.github.com>
2022-02-16 18:04:34 +0700

    add 4 website

---
+++ Courses Web/js/menuBar.js
@@ -0,0 +1,8 @@
+var MenuList = document.getElementById("navlinks");
+function openMenu() {
+  MenuList.style.right = "0";
+}
+
+function closeMenu() {
+  MenuList.style.right = "-200px";
+}
---
+++ Restaurant Website/js/nav.js
@@ -0,0 +1,8 @@
+var MenuList = document.getElementById("navlinks");
+function openMenu() {
+  MenuList.style.top = "0";
+}
+
+function closeMenu() {
+  MenuList.style.top = "-1000px";
+}
